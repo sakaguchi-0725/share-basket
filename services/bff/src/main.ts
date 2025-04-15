193ea9382d24c6e390b59a8097d35dbf4e94f651
Kazuma Sakaguchi <sakaguchi@KazumanoMacBook-Pro.local>
2025-04-15 22:32:54 +0900

    アクセストークンをCookieに設定する

--- services/bff/src/main.ts
+++ services/bff/src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { GrpcExceptionFilter } from './common/error'
+import * as cookieParser from 'cookie-parser'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.useGlobalFilters(new GrpcExceptionFilter())
+  app.use(cookieParser())
+
+  app.enableCors({
+    origin: true, // 開発環境では全オリジンを許可
+    credentials: true,
+  })
+
   await app.listen(process.env.PORT ?? 3000)
 }
 void bootstrap()
